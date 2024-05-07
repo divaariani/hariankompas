9550cd96baa96dd02848c7c060bf06b13364be1a
divaariani <divaarifinb@gmail.com>
2024-05-07 13:31:57 +0700

    add register page

---
+++ hariankompas/Pages/Register.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hariankompas.Pages;
+
+public class RegisterModel : PageModel
+{
+    private readonly ILogger<RegisterModel> _logger;
+
+    public RegisterModel(ILogger<RegisterModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
+
