f77bd6593212d939a1a163c9fa22038d20b798d9
divaariani <divaarifinb@gmail.com>
2024-05-08 11:13:51 +0700

    practice

---
+++ Pages/Home.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace hariankompas.Pages;
+
+public class HomeModel : PageModel
+{
+    private readonly ILogger<HomeModel> _logger;
+
+    public HomeModel(ILogger<HomeModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
