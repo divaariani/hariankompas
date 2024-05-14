4b085cb66f46495d06f5290b94b4c5c833775111
divaariani <divaarifinb@gmail.com>
2024-05-14 15:00:43 +0700

    add controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace hariankompas.Controllers;
+
+public class HomeController
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
