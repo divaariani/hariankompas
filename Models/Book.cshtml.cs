328d20541b433ea5a288c2f8e11f23f448a8a21f
divaariani <divaarifinb@gmail.com>
2024-05-15 13:41:13 +0700

    book format feature

---
+++ Models/Book.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace hariankompas.Pages;
+
+public class BookModel : PageModel
+{
+    private readonly IHttpClientFactory _clientFactory;
+
+    public BookModel(IHttpClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+
+    public async Task OnGet()
+    {
+        var client = _clientFactory.CreateClient();
+        var response = await client.GetAsync("http://localhost:8000/api/BookFormat/List?page=1&pageSize=20");
+
+        if (response.IsSuccessStatusCode)
+        {
+            var jsonString = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<BookData>(jsonString);
+
+            ViewData["Rows"] = data.Rows;
+        }
+        else
+        {
+            // Handle if the request failed
+        }
+    }
+
+}
+
+public class BookData
+{
+    public List<BookRow>? Rows { get; set; }
+    public int Status { get; set; }
+    public string? Message { get; set; }
+    public string? MessageDescription { get; set; }
+    public int RowCount { get; set; }
+    public List<string>? Columns { get; set; }
+}
+
+public class BookRow
+{
+    public int RowNumber { get; set; }
+    public int RowCount { get; set; }
+    public int ID { get; set; }
+    public string? Code { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public int ModifiedBy { get; set; }
+}
---
+++ Models/BookDetail.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace hariankompas.Pages
+{
+    public class BookDetailModel : PageModel
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public BookDetailModel(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public BookRow Row { get; private set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            var client = _clientFactory.CreateClient();
+            var response = await client.GetAsync($"http://localhost:8000/api/BookFormat/Read?ID={id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<BookData>(jsonString);
+
+                if (data.Rows.Count > 0)
+                {
+                    Row = data.Rows[0];
+                    return Page();
+                }
+            }
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
