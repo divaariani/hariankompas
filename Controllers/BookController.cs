46aa664dcaf3b5d524f112a37da45169c98b4061
divaariani <divaarifinb@gmail.com>
2024-05-16 16:17:42 +0700

    add new book

---
+++ Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using hariankompas.Pages;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class BookController : Controller
+{
+    private readonly HttpClient _httpClient;
+
+    public BookController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(BookAddModel model)
+    {
+        model.Id = 0;
+
+        var json = JsonSerializer.Serialize(model);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("http://localhost:8000/api/BookFormat/Save", content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        else
+        {
+            return RedirectToAction("Error", "Home");
+        }
+    }
+}
